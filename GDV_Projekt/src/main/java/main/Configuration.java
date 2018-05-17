a48b486d57c32683ccdf468907f90267164eea29
toadette77 <toadette007@outlook.de>
2018-05-17 13:24:19 +0200

    configuration relative Pfade

--- GDV_Projekt/src/main/java/main/Configuration.java
+++ GDV_Projekt/src/main/java/main/Configuration.java
@@ -1,13 +1,15 @@
 package main;
 
+import java.io.File;
+
 import de.fhpotsdam.unfolding.providers.AbstractMapProvider;
 import de.fhpotsdam.unfolding.providers.Microsoft;
 
 public class Configuration {
 
 	public static AbstractMapProvider provider = new Microsoft.RoadProvider();
-	public static String districtPath = "C:\\Users\\Thorsten\\Git\\GDV\\Projekt\\GDV_Projekt\\src\\main\\resources\\districts.geo.json";
-	public static String locationPath = "C:\\Users\\Thorsten\\Git\\GDV\\Projekt\\GDV_Projekt\\src\\main\\resources\\filmlocation.csv";
+	public static String districtPath = new File("").getAbsolutePath()+"/../../src/main/resources/districts.geo.json";
+	public static String locationPath = new File("").getAbsolutePath()+"/../../src/main/resources/filmlocation.csv";
 	public static int windowWidth = 1920;
 	public static int windowsHeight = 1080;
 }
